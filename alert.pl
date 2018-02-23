c011a291ec18d177167aa68ed439e3b16925d564
balakine <balakine@users.noreply.github.com>
2018-02-22 16:40:21 -0800

    Create alert.pl

---
+++ alert.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use CGI;
+print "HTTP/1.0 200 OK\n"; # HTTPi hack
+$q = CGI->new;
+print $q->header(-type=>'application/json', -connection=>'close');
+print '{}';
+
+my $subject = $q->param("subject");
+my $body = $q->param("body");
+system("echo \"$body\" | mail -s \"$subject\" vbalakine\@bestbuycanada.ca");
+
+return 1;
